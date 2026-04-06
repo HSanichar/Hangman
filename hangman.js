68cc8765ecabb6d3307c4601cb713ce8900f6a46
H. Sanichar <hsanichar2006@gmail.com>
2026-04-06 19:00:14 -0400

    added more to js and html

--- hangman.js
+++ hangman.js
@@ -1,20 +1,38 @@
 const POSSIBLE_WORDS = ["amity", "candor", "dauntless", 
     "erudite", "abnegation", "padawan"];
+    var word = "";
+    var guesses = "";
 
 
-    let newGae = function() {
+    let newGame = function(){
 
         let randomIndex = parseInt((Math.random() * POSSIBLE_WORDS.length));
-        let word = POSSIBLE_WORDS[randomIndex];
-        console.log("Word Chosen: ", word);
-
-
+        word = POSSIBLE_WORDS[randomIndex];
+        updatePage();
+    }
+    let updatePage = function() {
         let clueString = "";
-        for(let i = 0; i < word.length; i++) 
-            {
+        for(let i = 0; i < word.length; i++){
+                var currentLetter = word.charAt(i);
+                if(guesses.indexOf(currentLetter) >= 0) {
+                    clueString += currentLetter+ " ";
+                } 
+                else {
                 clueString += "_";
             }
+        }
 
             let clue = document.getElementById("clue");
             clue.textContent = clueString;
-        }
+
+            let guessArea = document.getElementById("guesses");
+            guessArea.textContent = "Guesses: " + guesses;
+    }
+    
+    let gueesLetter = function() {
+        let input = document.getElementById("guess");
+        let letter = input.value;
+        guesses += letter;
+        updatePage();
+    }
+    
