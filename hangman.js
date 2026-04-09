f09253e43fb1868005b68fdb30899d0d03824e72
H. Sanichar <hsanichar2006@gmail.com>
2026-04-09 18:17:43 -0400

    finishing quest

--- hangman.js
+++ hangman.js
@@ -1,49 +1,92 @@
-const POSSIBLE_WORDS = ["amity", "candor", "dauntless", 
-    "erudite", "abnegation", "padawan"];
-    var word = "";
-    var guesses = "";
+const POSSIBLE_WORDS = ["amity", "candor", "dauntless", "erudite", "abnegation", "padawan"]; 
 
+var word = ""; 
+var guesses = ""; 
+var guessCount = 6; 
+var gameOver = false; 
 
-    let newGame = function(){
+let newGame = function(){ 
+    let randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length); 
+    word = POSSIBLE_WORDS[randomIndex]; 
+    guesses = ""; 
+    guessCount = 6; 
+    gameOver = false; 
+    updatePage(); 
+} 
 
-        let randomIndex = parseInt((Math.random() * POSSIBLE_WORDS.length));
-        word = POSSIBLE_WORDS[randomIndex];
-        guesses = "";
-        updatePage();
-    }
-    let updatePage = function() {
-        let clueString = "";
-        for(let i = 0; i < word.length; i++){
-                var currentLetter = word.charAt(i);
-                if(guesses.indexOf(currentLetter) >= 0) {
-                    clueString += currentLetter+ " ";
-                } 
-                else {
-                clueString += "_";
-            }
-        }
+let updatePage = function() { 
 
-            let clue = document.getElementById("clue");
-            clue.textContent = clueString;
+    let clueString = ""; 
+    let won = true; 
 
-            let guessArea = document.getElementById("guesses");
-            guessArea.textContent = "Guesses: " + guesses;
+    for(let i = 0; i < word.length; i++){ 
+        let currentLetter = word.charAt(i); 
 
-            let image = document.getElementById("hangmanpic");
-            image.src = `images/hangman${guessCount}.gif`;
-    }
+        if(guesses.indexOf(currentLetter) >= 0) { 
+            clueString += currentLetter + " "; 
+        } else { 
+            clueString += "_ "; 
+            won = false; 
+        } 
+    } 
 
-    let guessLetter = function() {
-        let input = document.getElementById("guess");
-        let letter = input.value;
-        letter = letter.toLowerCase();
-        if(word.indexOf(letter) < 0) {
-            guessCount--;
-        }
-        else {
-        }
-        guesses += letter;
-        input.value = "";
-        updatePage();
-    }
-    
+    let clue = document.getElementById("clue"); 
+    clue.textContent = clueString; 
+
+    let guessArea = document.getElementById("guesses"); 
+
+    // Win
+    if(won && word !== "") { 
+        guessArea.textContent = "You WIN! Word was: " + word; 
+        gameOver = true; 
+    } 
+    // Lose
+    else if(guessCount <= 0) { 
+        guessArea.textContent = "You LOST! Word was: " + word; 
+        gameOver = true; 
+    } 
+    else { 
+        guessArea.textContent = "Guesses: " + guesses + " | Lives: " + guessCount; 
+    } 
+
+    let image = document.getElementById("hangmanpic"); 
+    image.src = `images/hangman${guessCount}.gif`; 
+} 
+
+let guessLetter = function() { 
+
+    // Don't allow before game starts
+    if(word === "") { 
+        return; 
+    } 
+
+    // Don't allow after game ends
+    if(gameOver) { 
+        return; 
+    } 
+
+    let input = document.getElementById("guess"); 
+    let letter = input.value.toLowerCase(); 
+
+    // Clear input always
+    input.value = ""; 
+
+    // Prevent empty input
+    if(letter === "") { 
+        return; 
+    } 
+
+    // Prevent duplicate guesses
+    if(guesses.indexOf(letter) >= 0) { 
+        return; 
+    } 
+
+    // Wrong guess
+    if(word.indexOf(letter) < 0) { 
+        guessCount--; 
+    } 
+
+    guesses += letter; 
+
+    updatePage(); 
+}
